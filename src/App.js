be728a3b58a07290e14fe1cbe0d3181f70fb7731
Shivagiii <shivangiadeshara@gmail.com>
2024-02-25 13:12:54 +0530

    corrections

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   })
   const [display,setDisplay]= useState(null);
   function handleChange(e){
+    setDisplay('')
     setFullName({
       ...fullName,
       [e.target.name]:e.target.value,
@@ -33,15 +34,15 @@ console.log(fullName);
       <h2>Full Name Display</h2>
       <form onSubmit={handleSubmit}  >
         <div><label for='firstName'>First Name:</label>
-          <input id='firstName' name='firstName' value={fullName.firstName} onChange={handleChange} required/>       
+          <input type="text" id='firstName' name='firstName' value={fullName.firstName} onChange={handleChange} required/>       
         </div>
         <div>
         <label for='lastName'>Last Name:</label>
-        <input id='lastName' name='lastName'  value={fullName.lastName} onChange={handleChange} required/>
+        <input type="text" id='lastName' name='lastName'  value={fullName.lastName} onChange={handleChange} required/>
         </div>
         <button type='submit'>Submit</button>
       </form>
-       {display ? (<p>FullName: {display}</p>):(<></>) }
+       {display ? (<p>Full Name: {display}</p>):(<></>) }
     </div>
   );
 }
